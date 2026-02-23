63b40fbdbddcdea533c54c4094faa8fcfa6a2faa
Pieter Ytsma <pieterytsma@proton.me>
2026-02-23 21:57:44 +0100

    versienummer toegevoegd

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import sys
 from PySide6.QtWidgets import QApplication
 from ui import FlashcardApp
 
+__version__ = "1.0.0"
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = FlashcardApp()
+    window.setWindowTitle(f"Flashcard App v{__version__}")
     window.show()
     sys.exit(app.exec())
